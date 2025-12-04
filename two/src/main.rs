5af124717b962d0b7cfedc3f301f6592e691945a
Liam Davey <104245487+LiamDavey@users.noreply.github.com>
2025-12-04 18:41:32 +1000

    added test and tidied up a bit

--- two/src/main.rs
+++ two/src/main.rs
@@ -4,16 +4,16 @@ use std::fs;
 fn p1(id_ranges: Vec<&str>) -> i64 {
     let mut ans = 0;
     for id_range in id_ranges {
-        let ids: Vec<&str> = id_range.split("-").collect();
-        let start: i64 = ids[0].parse().unwrap();
-        let end: i64 = ids[1].parse().unwrap();
+        let (start, end) = id_range.split_once('-').unwrap();
+        let start: i64 = start.parse().unwrap();
+        let end: i64 = end.parse().unwrap();
         for id in start..=end {
-            let id_str: String = id.to_string();
-            let id_len: usize = id_str.chars().count();
+            let id_str = id.to_string();
+            let id_len = id_str.chars().count();
             if id_len % 2 != 0 {
                 continue;
             }
-            let (left, right) = id_str.split_at(id_len/2);
+            let (left, right) = id_str.split_at(id_len / 2);
             if left == right {
                 ans += id;
             }
@@ -22,6 +22,28 @@ fn p1(id_ranges: Vec<&str>) -> i64 {
     ans
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_p1() {
+        let id_ranges = vec![
+            "11-22",
+            "95-115",
+            "998-1012",
+            "1188511880-1188511890",
+            "222220-222224",
+            "1698522-1698528",
+            "446443-446449",
+            "38593856-38593862",
+            "565653-565659",
+            "824824821-824824827",
+            "2121212118-2121212124",
+        ];
+        assert_eq!(p1(id_ranges), 1227775554);
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -33,7 +55,7 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Path should be a readable UTF-8 file");
     let id_ranges: Vec<&str> = contents.split(",").collect();
 
-    let p1 = p1(id_ranges);
+    let p1_ans = p1(id_ranges);
 
-    println!("p1 password is {p1}!");
+    println!("p1 answer is {p1_ans}!");
 }
