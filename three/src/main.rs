6e07f68667167cc423984253f36205c2147a4e7f
Liam Davey <104245487+LiamDavey@users.noreply.github.com>
2025-12-12 06:10:28 +1000

    d3p2

--- three/src/main.rs
+++ three/src/main.rs
@@ -33,6 +33,27 @@ fn p1(input: &[u8]) -> usize {
     ans
 }
 
+fn p2(input: &[u8]) -> u128 {
+    let mut ans = 0;
+    for bank in input.split(|&b| b == b'\n') {
+        let mut largest_dozen: Vec<Battery> = vec![];
+        let mut next_option = 0;
+        while largest_dozen.len() < 12 {
+            let last_option = bank.len() - 12 + largest_dozen.len();
+            let options = &bank[next_option..=last_option];
+            let (pos, jolt) = options.iter().enumerate().rev().max_by_key(|x| x.1).unwrap();
+            largest_dozen.push(Battery { pos: pos+next_option, joltage: *jolt });
+            next_option += pos+1;
+        }
+        let mut bank_volts = 0;
+        for battery in largest_dozen {
+            bank_volts = bank_volts * 10 + u128::from(battery.joltage - b'0');
+        }
+        ans += bank_volts;
+    }
+    ans
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -41,9 +62,10 @@ fn main() {
 
     let file_path = &args[1];
     let contents = fs::read(file_path).expect("I can't read that file");
-    //let contents = fs::read_to_string(file_path).expect("Path should be a readable UTF-8 file");
 
     let p1_ans = p1(&contents);
+    let p2_ans = p2(&contents); 
 
     println!("p1 answer is {p1_ans}!");
+    println!("p2 answer is {p2_ans}!");
 }
