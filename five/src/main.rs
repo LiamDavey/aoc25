8c12832096956f4cf1abb5a2343b661edb345732
Liam Davey <104245487+LiamDavey@users.noreply.github.com>
2025-12-18 20:41:24 +1000

    day5p2

--- five/src/main.rs
+++ five/src/main.rs
@@ -1,5 +1,25 @@
 use std::fs;
 
+fn sort_ranges(ranges: &Vec<(usize, usize)>) -> Vec<(usize, usize)> {
+    let mut sorted = vec![];
+    let mut ranges = ranges.clone();
+    ranges.sort_by_key(|r| r.0);
+    let mut last_end: usize = 0;
+    for (start, end) in ranges {
+        if end <= last_end {
+            continue;
+        }
+        if start <= last_end {
+            let new_start = last_end + 1;
+            sorted.push((new_start, end));
+        } else {
+            sorted.push((start, end));
+        }
+        last_end = end;
+    }
+    sorted
+}
+
 fn main() {
     let contents =
         fs::read_to_string("src/actual.txt").expect("Path should be a readable UTF-8 file");
@@ -11,6 +31,7 @@ fn main() {
             (a.parse().unwrap(), b.parse().unwrap())
         })
         .collect();
+    let fresh_ranges = sort_ranges(&fresh_ranges);
     let mut p1_ans = 0;
     for item in avail.lines() {
         let item: usize = item.parse().unwrap();
@@ -20,4 +41,9 @@ fn main() {
         }
     }
     println!("p1 answer is {p1_ans}");
+    let mut p2_ans = 0;
+    for (start, end) in fresh_ranges {
+        p2_ans += end - start + 1;
+    }
+    println!("p2 answer is {p2_ans}");
 }
