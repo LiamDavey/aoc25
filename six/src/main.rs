56158661426bc01c1524c1a7e6957e6f939d3b74
Liam Davey <104245487+LiamDavey@users.noreply.github.com>
2025-12-23 12:52:31 +1000

    day6p2

--- six/src/main.rs
+++ six/src/main.rs
@@ -1,16 +1,23 @@
 use std::env;
 use std::fs;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        panic!("Usage: ./six <file_path>");
+enum Operator {
+    Product,
+    Sum,
+}
+
+fn solve(problem: &[usize], operator: Operator) -> usize {
+    match operator {
+        Operator::Product => problem.iter().product(),
+        Operator::Sum => problem.iter().sum(),
     }
+}
 
-    let file_path = &args[1];
-    let contents = fs::read_to_string(file_path).expect("I can't read that file");
-
-    let mut lines: Vec<Vec<&str>> = contents.lines().map(|line| line.split_whitespace().collect()).collect();
+fn p1(contents: &str) -> usize {
+    let mut lines: Vec<Vec<&str>> = contents
+        .lines()
+        .map(|line| line.split_whitespace().collect())
+        .collect();
 
     let mut transposed = vec![];
     while !lines[0].is_empty() {
@@ -23,16 +30,61 @@ fn main() {
         transposed.push(col);
     }
 
-    let mut p1_ans = 0;
+    let mut ans = 0;
     for mut problem in transposed {
-        let operator = problem.pop().unwrap();
-        let ans: usize = match operator {
-            "*" => problem.iter().map(|num| num.parse::<usize>().unwrap()).product(),
-            "+" => problem.iter().map(|num| num.parse::<usize>().unwrap()).sum(),
+        let operator = match problem.pop().unwrap() {
+            "*" => Operator::Product,
+            "+" => Operator::Sum,
             _ => unreachable!("Operator will only be * or +"),
-            
         };
-        p1_ans += ans;
+        let problem: Vec<usize> = problem
+            .iter()
+            .map(|num| num.parse::<usize>().unwrap())
+            .collect();
+        ans += solve(&problem, operator);
     }
+    ans
+}
+
+fn p2(contents: &str) -> usize {
+    let mut lines: Vec<Vec<char>> = contents
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect();
+
+    let mut ans = 0;
+    while !lines[0].is_empty() {
+        let mut problem: Vec<usize> = vec![];
+        let mut operator: Option<Operator> = None;
+        while operator.is_none() {
+            let mut col = vec![];
+            for line in &mut lines {
+                if let Some(x) = line.pop() {
+                    match x {
+                        '*' => operator = Some(Operator::Product),
+                        '+' => operator = Some(Operator::Sum),
+                        ' ' => (),
+                        x => col.push(x),
+                    }
+                }
+            }
+            if !col.is_empty() {
+                problem.push(col.iter().collect::<String>().parse().unwrap());
+            }
+        }
+        ans += solve(&problem, operator.unwrap());
+    }
+    ans
+}
+
+fn main() {
+    let file_path = env::args().nth(1).expect("Usage: six <file_path>");
+
+    let contents = fs::read_to_string(file_path).expect("I can't read that file");
+
+    let p1_ans = p1(&contents);
     println!("{p1_ans}");
+
+    let p2_ans = p2(&contents);
+    println!("{p2_ans}");
 }
